326e0fbab16500ae3af8646332c5804a760aa70b
MarcoC81 <145415396+MarcoC81@users.noreply.github.com>
2023-10-27 11:00:08 -0400

    Files
    files

---
+++ Memory Game/script.js
@@ -0,0 +1,9 @@
+class Card {
+    constructor(topic, imgLink, isTurned, isRevealed)
+    {
+        this.topic = topic;
+        this.imgLink  = imgLink;
+        this.isTurned = isTurned;
+        this.isRevealed = isRevealed;
+    }
+}
